f2329d303403276f3998f779253e371a8f1ade6d
vladar21 <vladar21@gmail.com>
2022-01-24 01:36:55 +0200

    setting tailwindcss, creating app.tsx

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './components/**/*.{html,tsx}',
+    './pages/**/*.{html,tsx}',
+    './index.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
